654e3cec4ce93913ab9b40d03625347c3911231a
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-01-23 22:53:59 +0600

    added env.ts

--- blog-site-client/src/env.ts
+++ blog-site-client/src/env.ts
@@ -1,6 +1,13 @@
 import { createEnv } from '@t3-oss/env-nextjs';
+import z from 'zod';
 export const env = createEnv({
-    server: {},
+    server: {
+        BACKEND_URL: z.url(),
+        FRONTEND_URL: z.url(),
+        API_URL: z.url(),
+        AUTH_URL: z.url()
+
+    },
     client: {},
     runtimeEnv: {}
 });
