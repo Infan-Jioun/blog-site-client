52ff3691799d765dcfc7a424a4c4e1d93617c391
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-01-24 09:33:31 +0600

    authenticated login

--- blog-site-client/src/proxy.ts
+++ blog-site-client/src/proxy.ts
@@ -1,7 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
+import { userService } from "./services/user.service";
+import { Roles } from "./constant/roles";
 
 export async function proxy(request: NextRequest) {
-
+    let isAuthenticated = false;
+    let isAdmin = false;
+    const response = await userService.getSesstion();
+    if (response && response.data) {
+        isAuthenticated = true;
+        isAdmin = response.data.user.role === Roles.admin
+    }
+    if (!isAuthenticated) {
+        return NextResponse.redirect(new URL("/login", request.url))
+    }
+    console.log(response?.data);
     return NextResponse.next();
 }
 export const config = {
