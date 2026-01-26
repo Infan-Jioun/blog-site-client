b44163ad5f4c40da8d1af206a0cb59fb2905b601
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-01-26 21:45:03 +0600

    added form

--- blog-site-client/src/app/(commonLayout)/Components/modules/signup-form.tsx
+++ blog-site-client/src/app/(commonLayout)/Components/modules/signup-form.tsx
@@ -32,33 +32,56 @@ export function SignupForm({
       e.preventDefault();
       form.handleSubmit();
     }} className={cn("flex flex-col gap-6", className)} {...props}>
+      <div className="flex flex-col items-center gap-1 text-center">
+        <h1 className="text-2xl font-bold">Create your account</h1>
+        <p className="text-muted-foreground text-sm text-balance">
+          Fill in the form below to create your account
+        </p>
+      </div>
       <FieldGroup>
-        <div className="flex flex-col items-center gap-1 text-center">
-          <h1 className="text-2xl font-bold">Create your account</h1>
-          <p className="text-muted-foreground text-sm text-balance">
-            Fill in the form below to create your account
-          </p>
-        </div>
-        <Field>
-          <FieldLabel htmlFor="name">Full Name</FieldLabel>
-          <Input id="name" type="text" placeholder="John Doe" required />
-        </Field>
-        <Field>
-          <FieldLabel htmlFor="email">Email</FieldLabel>
-          <Input id="email" type="email" placeholder="m@example.com" required />
-        </Field>
-        <Field>
-          <FieldLabel htmlFor="password">Password</FieldLabel>
-          <Input id="password" type="password" required />
-          <FieldDescription>
-            Must be at least 8 characters long.
-          </FieldDescription>
-        </Field>
-        <Field>
-          <FieldLabel htmlFor="confirm-password">Confirm Password</FieldLabel>
-          <Input id="confirm-password" type="password" required />
-          <FieldDescription>Please confirm your password.</FieldDescription>
-        </Field>
+        <form.Field name="name" children={(field) => {
+          return (
+            <>
+              <Field>
+                <FieldLabel htmlFor={field.name}>Full Name</FieldLabel>
+                <Input id={field.name} name={field.name} type="text" placeholder="John Doe" value={field.state.value}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                  required />
+              </Field>
+              <Field></Field>
+            </>
+          )
+        }} />
+        <form.Field name="email" children={(field) => {
+          return (
+            <>
+              <Field>
+                <FieldLabel htmlFor={field.name}>Your Email</FieldLabel>
+                <Input id={field.name} name={field.name} type="text" placeholder="Enter Your email" value={field.state.value}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                  required />
+              </Field>
+              <Field></Field>
+            </>
+          )
+        }} />
+        <form.Field name="password" children={(field) => {
+          return (
+            <>
+              <Field>
+                <FieldLabel htmlFor={field.name}>Password</FieldLabel>
+                <Input id={field.name} name={field.name} type="password" placeholder="password" value={field.state.value}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                  required />
+              </Field>
+              <Field></Field>
+            </>
+          )
+        }} />
+
+
+
+
         <Field>
           <Button type="submit">Create Account</Button>
         </Field>
