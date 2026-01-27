9d9155abc5536e419c1190c008f5189b94570075
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-01-27 15:46:55 +0600

    createBlogServerForm create

--- blog-site-client/src/app/(dashboardLayout)/@user/dashboard/create-blog/page.tsx
+++ blog-site-client/src/app/(dashboardLayout)/@user/dashboard/create-blog/page.tsx
@@ -1,7 +1,8 @@
+import CreateBlogServerForm from '@/app/(dashboardLayout)/components/modules/user/createBlog/CreateBlogServerForm'
 import React from 'react'
 
 export default function CreateBlog() {
   return (
-    <div>CreateBlog</div>
+    <div><CreateBlogServerForm /></div>
   )
 }
---
+++ blog-site-client/src/app/(dashboardLayout)/components/modules/user/createBlog/CreateBlogServerForm.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CreateBlogServerForm() {
+  return (
+    <div>CreateBlogServerForm</div>
+  )
+}
