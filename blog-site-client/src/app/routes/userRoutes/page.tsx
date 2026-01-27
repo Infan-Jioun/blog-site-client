474dd052bc411833ceeb2c4dd546ef3f17a7e7a2
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-01-27 14:39:17 +0600

    added userRoutes

--- blog-site-client/src/app/routes/userRoutes/page.tsx
+++ blog-site-client/src/app/routes/userRoutes/page.tsx
@@ -1,4 +1,14 @@
 import { Route } from '@/types'
 import React from 'react'
 
-export const userRoutes: Route
+export const userRoutes: Route[] = [
+    {
+        title: "Blog Management",
+        items: [
+            {
+                title: "Create blog",
+                url: "/dashboard/create-blog"
+            }
+        ]
+    }
+]
